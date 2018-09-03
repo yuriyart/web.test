07870104ef46ec87334728033ccb073a14768744
varajan <30838113+varajan@users.noreply.github.com>
2018-09-03 23:09:06 +0300

    Initial checkin

---
+++ Callculator/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Callculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Callculator/Controllers/SettingsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Callculator.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
